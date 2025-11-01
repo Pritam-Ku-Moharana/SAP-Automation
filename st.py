8dfe2631a34bb9dfa3430aa88b6872930ecc793b
Pritam Kumar Moharana <152791655+Pritam-Ku-Moharana@users.noreply.github.com>
2025-11-01 14:47:43 +0530

    st.py

--- st.py
+++ st.py
@@ -4,27 +4,51 @@ from annotated_text import annotated_text
 # hardcoded demo users
 USERS = {
     "pritam": "12345",
-    "admin": "admin123"
+    "ayush": "1"
 }
 
-st.title("Login Page")
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
 
-username = st.text_input("Username")
-password = st.text_input("Password", type="password")
-
-if st.button("Login"):
-    if username in USERS and USERS[username] == password:
-        st.success("Login Successful ✅")
-        st.session_state["logged_in"] = True
-        st.session_state["user"] = username
-    else:
-        st.error("Invalid username or password ❌")
-
+##############################################################################################################################
+def login():
+    st.title("Login Page")
+    
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    
+    if st.button("Login"):
+        if username in USERS and USERS[username] == password:
+            st.success("Login Successful ✅")
+            st.session_state["logged_in"] = True
+            st.session_state["user"] = username
+        else:
+            st.error("Invalid username or password ❌")
+##############################################################################################################################
 
 # if logged in → show next page
 if "logged_in" in st.session_state and st.session_state["logged_in"]:
     st.write(f"Welcome {st.session_state['user']}")
     st.write("Now you can see the dashboard here...")
+
+    if st.button("Logout"):
+        st.session_state.logged_in = False
+else:
+    login()
+    
+pages = st.sidebar.selectbox("Pages", ["Home","Attendance","Logs"])
+
+if pages == "Home":
+    st.write("This is home page")
+
+elif pages == "Attendance":
+    st.write("attendance page here")
+
+elif pages == "Logs":
+    st.write("logs page here")
+
+
+
 
 # REMOVE STREAMLIT STYLE (menu, footer, header)
 hide_style = """
@@ -35,6 +59,7 @@ header {visibility: hidden;}
 </style>
 """
 st.markdown(hide_style, unsafe_allow_html=True)
+
 
 
 
