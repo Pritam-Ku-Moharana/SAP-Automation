ba0a8b70272a65c8b4152b48a855bd3617ff3b59
Pritam Kumar Moharana <152791655+Pritam-Ku-Moharana@users.noreply.github.com>
2025-11-01 12:12:42 +0530

    st.py

--- st.py
+++ st.py
@@ -1,5 +1,26 @@
 import streamlit as st
 
-st.title("Hello from Streamlit!")
-st.write("This app is running from Anaconda Command Line")
+# hardcoded demo users
+USERS = {
+    "pritam": "12345",
+    "admin": "admin123"
+}
 
+st.title("Login Page")
+
+username = st.text_input("Username")
+password = st.text_input("Password", type="password")
+
+if st.button("Login"):
+    if username in USERS and USERS[username] == password:
+        st.success("Login Successful ✅")
+        st.session_state["logged_in"] = True
+        st.session_state["user"] = username
+    else:
+        st.error("Invalid username or password ❌")
+
+
+# if logged in → show next page
+if "logged_in" in st.session_state and st.session_state["logged_in"]:
+    st.write(f"Welcome {st.session_state['user']}")
+    st.write("Now you can see the dashboard here...")
