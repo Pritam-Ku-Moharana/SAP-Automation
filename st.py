4d09f19daebc4bd8bb51deb624215d3a5fc9d35a
Pritam Kumar Moharana <152791655+Pritam-Ku-Moharana@users.noreply.github.com>
2025-11-03 17:14:21 +0530

    st.py

--- st.py
+++ st.py
@@ -28,9 +28,26 @@ def login():
 def main_page():
     st.title("Enter the details")
 
-    reporting_period_frm = st.number_input("Reporting Period From", step=1, format="%d")
+    reporting_year = st.number_input("Reporting Year", min_value=2000, max_value=2100, step=1, format="%d")
+    st.write("Reporting Year:", reporting_year)
+        
+    reporting_period_frm = st.number_input("Reporting Period From", min_value=1, max_value=12, step=1, format="%d")
+    st.write("Reporting Period From:", reporting_period_frm)
 
-    st.write("You entered:", reporting_period_frm)
+    reporting_period_to = st.number_input("Reporting Period To", min_value=1, max_value=12, step=1, format="%d")
+    st.write("Reporting Period To:",reporting_period_to)
+
+    comparison_year = st.number_input("Comparison Year", min_value=2000, max_value=2100, step=1, format="%d")
+    st.write("Comparison Year",comparison_year)
+
+    comaprison_period_from = st.number_input("Comparison Per. From", min_value=1, max_value=12, step=1, format="%d")
+    st.write("Comparison Period From",comaprison_period_from)
+    
+    comparison_period_to = st.number_input("Comparison Period To", min_value=1, max_value=12, step=1, format="%d")
+    st.write("Comparison Period To",comparison_period_to)
+
+
+
 
     if st.button("Logout"):
         st.session_state.logged_in = False
@@ -51,3 +68,4 @@ footer {visibility:hidden;}
 header {visibility:hidden;}
 </style>
 """, unsafe_allow_html=True)
+
